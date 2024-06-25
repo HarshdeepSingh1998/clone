03a65c90cad4795f27112f1492509f54dcc27575
harshdeepsingh98 <harshdeep2498@gmail.com>
2024-06-25 23:55:37 +0530

    feat: 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout from 'components/Layout'
+import {
+  PageNotFoundContainer,
+  PageNotFoundDescription,
+  PageNotFoundHeader,
+  PageNotFoundLink
+} from '@/styles/Pages/404'
 
 const NotFoundPage: React.FC = () => {
   return (
-    <Layout>
-      <div
-        style={{
-          textAlign: 'center',
-          height: '250px',
-          padding: '2rem',
-          background: '#c3bfbf'
-        }}
-      >
-        <h1 style={{ color: '#3c3535', padding: '2rem' }}>
-          404 - Page Not Found
-        </h1>
-        <p style={{ color: '#555', padding: '2rem' }}>
+    <Layout
+      background="radial-gradient(
+      79.14% 79.14% at 11% 98%,
+      /* Set background position to center */ #512709 -58.18%,
+      #000513 53.58%
+    );"
+    >
+      <PageNotFoundContainer>
+        <PageNotFoundHeader>404 - Page Not Found</PageNotFoundHeader>
+        <PageNotFoundDescription>
           Sorry, the page you are looking for does not exist.
-        </p>
+        </PageNotFoundDescription>
         <Link href="/">
-          <p
+          <PageNotFoundLink
             style={{
               color: '#111211',
               textDecoration: 'underline',
@@ -28,9 +31,9 @@ const NotFoundPage: React.FC = () => {
             }}
           >
             Go back to the home page
-          </p>
+          </PageNotFoundLink>
         </Link>
-      </div>
+      </PageNotFoundContainer>
     </Layout>
   )
 }
---
+++ styles/Pages/404/index.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const PageNotFoundContainer = styled.div`
+  text-align: center;
+  height: 250px;
+  padding: 2rem;
+  background: #c3bfbf;
+`
+
+export const PageNotFoundHeader = styled.div`
+  color: #3c3535;
+  padding: 2rem;
+`
+export const PageNotFoundDescription = styled.div`
+  color: #555;
+  padding: 2rem;
+`
+export const PageNotFoundLink = styled.div`
+  color: #111211;
+  textdecoration: underline;
+  paddingbottom: 2rem;
+`
