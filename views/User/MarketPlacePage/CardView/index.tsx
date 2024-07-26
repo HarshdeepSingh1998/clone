7ae61a3ae6655d5cc9980496bbdf2a9f6d9803b7
Harshdeep Singh <harshdeep.singh@thewitslab.com>
2024-07-27 00:05:30 +0530

    feat: feat: buy now modal

--- views/User/MarketPlacePage/CardView/index.tsx
+++ views/User/MarketPlacePage/CardView/index.tsx
@@ -1,10 +1,8 @@
-import { useSelector } from 'react-redux'
 import { ProductList } from '@/utils/ApiTypes/ProductList'
 import useScreenType from '@/utils/Customhooks/useScreenType'
 import { MarketPlaceDataInterface } from 'views/User/MarketPlacePage/types'
 import Loader from '@/components/Loader'
 import ProductCard from '@/components/ProductCard'
-import { selectUser } from '@/store/userSlice'
 import { CardContainer } from '@/styles/Views/User/MarketPlacePage/Card'
 
 const CardView = ({
